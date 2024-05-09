a99953563e667f784633ae17e98b57d3d3e553e2
Manos <manoskakarakis@gmail.com>
2024-05-09 11:51:10 +0300

    nx upgrade to 19 and expo v50

--- apps/ui/src/app/(app)/_layout.tsx
+++ apps/ui/src/app/(app)/_layout.tsx
@@ -1,13 +1,10 @@
 import { Slot } from 'expo-router';
 import { Footer } from '../../components/layout/Footer';
-import TouchHandler from '../../components/TouchHandler';
 
 export default function AppLayout() {
   return (
     <>
-      <TouchHandler>
-        <Slot />
-      </TouchHandler>
+      <Slot />
       <Footer />
     </>
   );
